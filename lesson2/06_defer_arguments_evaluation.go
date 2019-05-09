ffa1de410133cc6b2b780006a406d19b001a008c
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-09 08:07:41 +0200

    Example 6: argument evaluation in defer

---
+++ lesson2/06_defer_arguments_evaluation.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func function(a []int) {
+	fmt.Printf("Defer %v\n", a)
+}
+
+func main() {
+	var x = []int{1, 2, 3}
+
+	fmt.Printf("Current x value = %v\n", x)
+	defer function(x)
+
+	x[0] = 0
+
+	fmt.Printf("Current x value = %v\n", x)
+	defer function(x)
+
+	x[1] = 0
+
+	fmt.Printf("Current x value = %v\n", x)
+	defer function(x)
+
+	x[2] = 0
+
+	fmt.Printf("Current x value = %v\n", x)
+}
