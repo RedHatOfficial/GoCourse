dc161c236f40d8b02e6b9833cbf75e95d541ae84
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-10 08:42:33 +0200

    Example 25: pointer to an array

---
+++ lesson2/25_pointer_to_array.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	month := [12]string{
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December"}
+
+	fmt.Println(month)
+
+	p_third := &month[2]
+	*p_third = "Březen"
+
+	fmt.Println(month)
+}
