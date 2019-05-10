f9c4e6a12c4433a5d6591c9b007cf93a6010291d
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-10 08:42:33 +0200

    Example 16: arrays

---
+++ lesson2/16_arrays.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a1 [10]byte
+	var a2 [10]int32
+	a3 := [10]int32{1, 10, 2, 9, 3, 8, 4, 7, 5, 6}
+
+	fmt.Printf("Array 1 length: %d\n", len(a1))
+	fmt.Printf("Array 2 length: %d\n", len(a2))
+	fmt.Printf("Array 3 length: %d\n", len(a3))
+
+	var a [10]int
+
+	fmt.Printf("Original array: %v\n", a)
+
+	for i := 0; i < len(a1); i++ {
+		a[i] = i * 2
+	}
+
+	fmt.Printf("Modified array: %v\n", a)
+
+	var matrix [10][10]float32
+	fmt.Printf("Matrix:    %v\n", matrix)
+}
