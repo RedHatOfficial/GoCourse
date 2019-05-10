85601975894a19d8909f7e40111b77455a29487a
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-10 08:42:33 +0200

    Example 20: making slice from another slice

---
+++ lesson2/20_slice_from_slice.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a [10]int
+
+	slice1 := a[4:9]
+	slice2 := slice1[3:]
+
+	fmt.Printf("Array:            %v\n", a)
+	fmt.Printf("Array length:     %d\n\n", len(a))
+
+	fmt.Printf("Slice 1:          %v\n", slice1)
+	fmt.Printf("Slice 1 length:   %d\n", len(slice1))
+	fmt.Printf("Slice 1 capacity: %d\n\n", cap(slice1))
+
+	fmt.Printf("Slice 2:          %v\n", slice2)
+	fmt.Printf("Slice 2 length:   %d\n", len(slice2))
+	fmt.Printf("Slice 2 capacity: %d\n\n", cap(slice2))
+
+	slice2[0] = 99
+	slice2[1] = 99
+
+	fmt.Printf("Array:            %v\n", a)
+	fmt.Printf("Slice 1:          %v\n", slice1)
+	fmt.Printf("Slice 2:          %v\n", slice2)
+}
