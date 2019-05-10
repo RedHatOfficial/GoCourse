ef84cb50a40d0c989336edea57875fa395981f42
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-10 08:42:33 +0200

    Example 15: pass structures to functions

---
+++ lesson2/15_print_user.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type User struct {
+	id      uint32
+	name    string
+	surname string
+}
+
+func printUser(u User) {
+	fmt.Println(u)
+}
+
+func main() {
+	user1 := User{
+		id:      1,
+		name:    "Linus",
+		surname: "Torvalds"}
+
+	printUser(user1)
+
+	var user2 User
+	user2.id = 1
+	user2.name = "Steve"
+	user2.surname = "Ballmer"
+
+	printUser(user2)
+}
