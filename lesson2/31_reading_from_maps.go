d5095db0c3ac774ab7b9892516ec770b72c1f7dd
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-13 08:09:54 +0200

    Example 31: reading values from maps

---
+++ lesson2/31_reading_from_maps.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	m1 := make(map[string]int)
+	fmt.Println(m1)
+
+	m1["one"] = 0
+	m1["two"] = 1
+	m1["three"] = 2
+
+	fmt.Println(m1)
+
+	value, exist := m1["two"]
+
+	if exist {
+		fmt.Println("Found:", value)
+	} else {
+		fmt.Println("Not found")
+	}
+
+	value, exist = m1["thousand"]
+
+	if exist {
+		fmt.Println("Found:", value)
+	} else {
+		fmt.Println("Not found")
+	}
+}
