dbd290cd6698223003c4c0d38ddb653c45d1f4d7
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-13 08:09:54 +0200

    Example 29: map and struct(s)

---
+++ lesson2/29_map_and_struct.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type User struct {
+	id      uint32
+	name    string
+	surname string
+}
+
+func main() {
+	m1 := make(map[string]User)
+	fmt.Println(m1)
+
+	m1["1st"] = User{
+		id:      1,
+		name:    "Linus",
+		surname: "Torvalds"}
+
+	m1["2nd"] = User{
+		id:      2,
+		name:    "Rob",
+		surname: "Pike"}
+
+	fmt.Println(m1)
+}
