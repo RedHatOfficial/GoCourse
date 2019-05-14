c1dcfbb0f5c3fe75bbdaa810de880cd8e7ea4a2f
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-14 08:27:32 +0200

    Example #5: function type

---
+++ lesson3/05_function_type.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+type BinaryOp func(x, y int) int
+
+func applyBinaryOp(a int, b int, operation BinaryOp) int {
+	return operation(a, b)
+}
+
+func testBinaryOps(a int, b int) {
+	f1 := func(a int, b int) int { return a + b }
+	f2 := func(a int, b int) int { return a - b }
+	f3 := func(a int, b int) int { return a * b }
+	f4 := func(a int, b int) int { return a / b }
+	fmt.Printf("%d + %d = %d\n", a, b, applyBinaryOp(a, b, f1))
+	fmt.Printf("%d - %d = %d\n", a, b, applyBinaryOp(a, b, f2))
+	fmt.Printf("%d * %d = %d\n", a, b, applyBinaryOp(a, b, f3))
+	fmt.Printf("%d / %d = %d\n", a, b, applyBinaryOp(a, b, f4))
+}
+
+func main() {
+	testBinaryOps(1, 2)
+}
