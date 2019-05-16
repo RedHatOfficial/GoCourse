00a39bf8427e34e1d6678778527bdda6b47ee503
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-16 08:23:33 +0200

    Example #17: empty interface

---
+++ lesson3/17_empty_interface.go
@@ -0,0 +1,17 @@
+package main
+
+type I interface {}
+
+type T struct {}
+
+func (T) M() {
+	println("Nothing happened...")
+}
+
+func main() {
+	var i I = T{}
+	println(i)
+
+	var t T = T{}
+	t.M()
+}
