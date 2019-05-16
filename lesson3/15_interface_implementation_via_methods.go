ef3abd408c496830209e8a5e08a7637a403b0269
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-16 08:24:13 +0200

    Example #15: interface implementation via methods

---
+++ lesson3/15_interface_implementation_via_methods.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type OpenShape interface {
+	length() float64
+}
+
+type Line struct {
+	x1, y1 float64
+	x2, y2 float64
+}
+
+func length(shape OpenShape) float64 {
+	return shape.length()
+}
+
+func (line Line) length() float64 {
+	return math.Hypot(line.x1-line.x2, line.y1-line.y2)
+}
+
+func main() {
+	line1 := Line{x1: 0, y1: 0, x2: 100, y2: 100}
+
+	fmt.Println(line1)
+
+	line_length := length(line1)
+	fmt.Println(line_length)
+}
