0fa0803ca52947d3d769d2b5d568046247ac7c55
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-16 08:24:33 +0200

    Example #12: more methods for a given user type

---
+++ lesson3/12_more_methods.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type Line struct {
+	x1, y1 float64
+	x2, y2 float64
+}
+
+func (line *Line) translate(dx, dy float64) {
+	fmt.Printf("Translating line %v by %f %f\n", *line, dx, dy)
+}
+
+func (line *Line) rotate(angle float64) {
+	fmt.Printf("Rotating line %v by %f\n", *line, angle)
+}
+
+func main() {
+	line1 := Line{x1: 0, y1: 0, x2: 100, y2: 100}
+	fmt.Println(line1)
+	line1.translate(5, 5)
+	fmt.Println(line1)
+	line1.rotate(90)
+	fmt.Println(line1)
+}
