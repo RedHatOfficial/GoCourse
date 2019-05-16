e74390ca401d8cd0d728064e1482ee272f7cbb1d
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-16 08:25:01 +0200

    Example #14: missing interface implementation

---
+++ lesson3/14_interface_missing_implementation.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type OpenShape interface {
+	length() float64
+}
+
+type Line struct {
+	x1, y1 float64
+	x2, y2 float64
+}
+
+func length(shape OpenShape) float64 {
+	return shape.length()
+}
+
+func main() {
+	line1 := Line{x1: 0, y1: 0, x2: 100, y2: 100}
+
+	fmt.Println(line1)
+
+	line_length := length(line1)
+	fmt.Println(line_length)
+}
