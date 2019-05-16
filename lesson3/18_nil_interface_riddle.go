5178f1ec0ba13f236c8bc2023cf7d8e3323d4168
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-16 08:23:21 +0200

    Example #18: nil interface 'riddle'

---
+++ lesson3/18_nil_interface_riddle.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type I interface {}
+
+type T struct{}
+
+func main() {
+	var t *T
+	if t == nil {
+		fmt.Println("t is nil")
+	} else {
+		fmt.Println("t is not nil")
+	}
+	var i I = t
+	if i == nil {
+		fmt.Println("i is nil")
+	} else {
+		fmt.Println("i is not nil")
+	}
+
+}
