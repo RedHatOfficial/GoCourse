58ba0d62a36d44772e546c0be63997149adac585
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-16 08:23:55 +0200

    Example #16: more implementations of one interface

---
+++ lesson3/16_more_interface_implementations.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type OpenShape interface {
+	length() float64
+}
+
+type ClosedShape interface {
+	area() float64
+}
+
+func length(shape OpenShape) float64 {
+	return shape.length()
+}
+
+func area(shape ClosedShape) float64 {
+	return shape.area()
+}
+
+type Line struct {
+	x1, y1 float64
+	x2, y2 float64
+}
+
+type Circle struct {
+	x, y   float64
+	radius float64
+}
+
+type Ellipse struct {
+	x, y float64
+	a, b float64
+}
+
+type Rectangle struct {
+	x, y          float64
+	width, height float64
+}
+
+func (line Line) length() float64 {
+	return math.Hypot(line.x1-line.x2, line.y1-line.y2)
+}
+
+func (rect Rectangle) area() float64 {
+	return rect.width * rect.height
+}
+
+func (circle Circle) area() float64 {
+	return math.Pi * circle.radius * circle.radius
+}
+
+func (ellipse Ellipse) area() float64 {
+	return math.Pi * ellipse.a * ellipse.b
+}
+
+func main() {
+	line1 := Line{x1: 0, y1: 0, x2: 100, y2: 100}
+	fmt.Println("Line")
+	fmt.Println(line1)
+	fmt.Println(length(line1))
+	fmt.Println(line1.length())
+	fmt.Println()
+
+	fmt.Println("Rectangle")
+	r := Rectangle{x: 0, y: 0, width: 100, height: 100}
+	fmt.Println(r)
+	fmt.Println(area(r))
+	fmt.Println(r.area())
+	fmt.Println()
+
+	// cont
+
+	fmt.Println("Circle")
+	c := Circle{x: 0, y: 0, radius: 100}
+	fmt.Println(c)
+	fmt.Println(area(c))
+	fmt.Println(c.area())
+	fmt.Println()
+
+	fmt.Println("Ellipse")
+	e := Ellipse{x: 0, y: 0, a: 100, b: 50}
+	fmt.Println(e)
+	fmt.Println(area(e))
+	fmt.Println(e.area())
+	fmt.Println()
+}
