6198c4392bf1a8f4a1343c51f0a36bb04efa7a47
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-11-05 09:06:26 +0100

    Go Convey-based BDD test

---
+++ testing/go_convey1/behaviour_test.go
@@ -0,0 +1,21 @@
+package factorial
+
+import(
+	"testing"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestFactorial(t *testing.T) {
+	Convey("0! should be equal 1", t, func() {
+		So(Factorial(0), ShouldEqual, 1)
+	})
+}
+
+func TestFactorial2(t *testing.T) {
+	Convey("10! should be greater than 1", t, func() {
+		So(Factorial(10), ShouldBeGreaterThan, 1)
+	})
+	Convey("10! should be between 1 and 10000000", t, func() {
+		So(Factorial(10), ShouldBeBetween, 1, 10000000)
+	})
+}
