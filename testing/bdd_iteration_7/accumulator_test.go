4ef99f443a76dd09eecda125a578317f87d597ff
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-11-14 09:04:30 +0100

    BDD iteration #7

---
+++ testing/bdd_iteration_7/accumulator_test.go
@@ -0,0 +1,35 @@
+package accumulator
+
+import (
+	"fmt"
+	"github.com/DATA-DOG/godog"
+)
+
+var testAccumulator *acc
+
+func iHaveAnAccumulatorWith(initialValue int) error {
+	testAccumulator.value = initialValue
+	return nil
+}
+
+func iAddToAccumulator(value int) error {
+	testAccumulator.accumulate(value)
+	return nil
+}
+
+func theAccumulatedResultShouldBe(expected int) error {
+	if testAccumulator.value == expected {
+		return nil
+	}
+	return fmt.Errorf("Incorrect accumulator value %d", testAccumulator.value)
+}
+
+func FeatureContext(s *godog.Suite) {
+	s.Step(`^I have an accumulator with (-?\d+)$`, iHaveAnAccumulatorWith)
+	s.Step(`^I add (-?\d+) to accumulator$`, iAddToAccumulator)
+	s.Step(`^the accumulated result should be (-?\d+)$`, theAccumulatedResultShouldBe)
+
+	s.BeforeScenario(func(interface{}) {
+		testAccumulator = &acc{}
+	})
+}
