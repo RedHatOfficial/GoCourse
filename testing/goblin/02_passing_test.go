54c242125ccaa2e1769f28463f2e7368d7f8533c
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-11-15 08:36:15 +0100

    Checking behaviour of + operator

---
+++ testing/goblin/02_passing_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	. "github.com/franela/goblin"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	g := Goblin(t)
+	g.Describe("Adder", func() {
+		g.It("Should add two numbers ", func() {
+			g.Assert(1 + 1).Equal(2)
+		})
+	})
+}
