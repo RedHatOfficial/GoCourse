a674bf503fd9874d03a1af8aacb9c948adbe1f96
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-11-15 08:36:15 +0100

    Nested 'Describe' blocks in one test function

---
+++ testing/goblin/07_nested_struct_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	. "github.com/franela/goblin"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	g := Goblin(t)
+	g.Describe("Adder", func() {
+		g.Describe("Positive numbers", func() {
+			g.It("Should add two numbers ", func() {
+				g.Assert(1 + 1).Equal(2)
+			})
+			g.It("Should add two numbers", func() {
+				g.Assert(2 + 2).Equal(4)
+			})
+			g.It("Should add two numbers", func() {
+				g.Assert(10 + 20).Equal(30)
+			})
+		})
+		g.Describe("Negative numbers", func() {
+			g.It("Should add two numbers ", func() {
+				g.Assert(1 + -1).Equal(0)
+			})
+			g.It("Should add two numbers", func() {
+				g.Assert(2 + -4).Equal(-2)
+			})
+			g.It("Should add two numbers", func() {
+				g.Assert(10 + -20).Equal(-10)
+			})
+		})
+	})
+	g.Describe("Multiplier", func() {
+		g.Describe("Positive numbers", func() {
+			g.It("Should multiply two numbers ", func() {
+				g.Assert(1 * 1).Equal(1)
+			})
+			g.It("Should multiply two numbers", func() {
+				g.Assert(2 * 2).Equal(4)
+			})
+			g.It("Should multiply two numbers", func() {
+				g.Assert(10 * 20).Equal(200)
+			})
+		})
+		g.Describe("Negative numbers", func() {
+			g.It("Should multiply two numbers ", func() {
+				g.Assert(1 * -1).Equal(-1)
+			})
+			g.It("Should multiply two numbers", func() {
+				g.Assert(2 * -4).Equal(-8)
+			})
+			g.It("Should multiply two numbers", func() {
+				g.Assert(10 * -20).Equal(-200)
+			})
+		})
+	})
+}
