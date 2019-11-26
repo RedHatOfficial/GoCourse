c22ca27216b8547b043eb7cc4c9a2295452d89ff
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-11-26 09:32:04 +0100

    Example 3: check output of 'uname' command

---
+++ testing/goexpect/03_check_uname_bsd.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"regexp"
+	"time"
+
+	"github.com/google/goexpect"
+)
+
+func main() {
+	child, _, err := expect.Spawn("uname", -1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer child.Close()
+
+	linuxRe := regexp.MustCompile("BSD")
+	s, match, err := child.Expect(linuxRe, time.Second)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Found: %s", s)
+	log.Printf("Matches: %v", match)
+}
