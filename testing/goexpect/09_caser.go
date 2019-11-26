f5fe1519f0e62b67c37c5a94281bc520bca7f9c3
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-11-26 09:32:04 +0100

    Example 9: Caser struct

---
+++ testing/goexpect/09_caser.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"regexp"
+	"time"
+
+	"github.com/google/goexpect"
+)
+
+func main() {
+	child, _, err := expect.Spawn("python", 2*time.Second)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer child.Close()
+
+	br, err := child.ExpectBatch([]expect.Batcher{
+		&expect.BCas{[]expect.Caser{
+			&expect.Case{R: regexp.MustCompile("Python 2"), T: expect.OK()},
+			&expect.Case{R: regexp.MustCompile("Python 3"), T: expect.OK()}}}}, time.Second)
+
+	err = child.Send("quit()\n")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("OK")
+	for _, b := range br {
+		log.Println(b.Output)
+	}
+}
