01a7d36074dbdbe2c0040e02b5bbfce4bf458da8
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-11-27 12:30:55 +0100

    Example #13: Python interpreter cooperation as unit test

---
+++ testing/goexpect/13_python_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"regexp"
+	"testing"
+	"time"
+
+	"github.com/google/goexpect"
+)
+
+func TestPythonInterpreter(t *testing.T) {
+	child, _, err := expect.Spawn("python", 2*time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("Python interpreter has been started")
+
+	defer child.Close()
+
+	_, m, err := child.Expect(regexp.MustCompile("Python ([23])"), 2*time.Second)
+
+	err = child.Send("quit()\n")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(m) < 1 {
+		t.Fatal("No match (should not happen")
+	}
+	version := m[1]
+	t.Log("Detected Python version:", version)
+}
