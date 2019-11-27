fd10fc3b608fbc6bfb15c48dfcdf6c8ac77c96ff
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-11-27 12:33:08 +0100

    Example #1: simple check for uname

---
+++ testing/go-expect/01_check_uname.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"time"
+
+	expect "github.com/Netflix/go-expect"
+)
+
+func main() {
+	console, err := expect.NewConsole(expect.WithStdout(os.Stdout))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer console.Close()
+
+	command := exec.Command("uname")
+	command.Stdin = console.Tty()
+	command.Stdout = console.Tty()
+	command.Stderr = console.Tty()
+
+	err = command.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	time.Sleep(time.Second)
+	console.ExpectString("Linux")
+
+	err = command.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
