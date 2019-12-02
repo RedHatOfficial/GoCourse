6f763f371b33b0936b317a9cee18eb644ace6956
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-12-02 09:24:17 +0100

    Homework - factorial with error checking

---
+++ homeworks/factorial2/factorial.go
@@ -0,0 +1,11 @@
+package main
+
+import "errors"
+
+func factorial(n int64) (int64, error) {
+	// enter your implementation there
+	// calculation:
+	// error for n < 0
+	// 1 for n == 0
+	// n! for n >= 1
+}
---
+++ homeworks/factorial2/factorial_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"testing"
+)
+
+type factorialEntry struct {
+	n           int64
+	expected    int64
+	should_fail bool
+}
+
+func TestFactorial(t *testing.T) {
+	var entries = []factorialEntry{
+		{0, 1, false},
+		{1, 1, false},
+		{2, 2, false},
+		{3, 6, false},
+		{9, 362880, false},
+		{10, 3628800, false},
+		{20, 2432902008176640000, false},
+		{-1, 0, true},
+		{-100, 99, true},
+	}
+	for _, entry := range entries {
+		computed, err := factorial(entry.n)
+		switch {
+		case err != nil:
+			if entry.should_fail {
+				t.Logf("invalid input detected correctly: %d", entry.n)
+			} else {
+				t.Errorf("error returned for valid input: %d", entry.n)
+			}
+		case computed != entry.expected:
+			t.Errorf("%d! != %d, but %d", entry.n, computed, entry.expected)
+		default:
+			t.Logf("factorial computer correctly for input %d", entry.n)
+		}
+	}
+}
