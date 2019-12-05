7a7286e94ee8f01eefe66afec7823bbf5fe1584e
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-12-05 09:17:09 +0100

    Factorial tests #5

---
+++ testing/gomega/factorial5/factorial.go
@@ -0,0 +1,18 @@
+package main
+
+import "errors"
+
+func Factorial(n int64) (int64, error) {
+	switch {
+	case n < 0:
+		return 0, errors.New("math: factorial of negative number?!?")
+	case n == 0:
+		return 1, nil
+	default:
+		ret, err := Factorial(n - 2)
+		if err != nil {
+			return 0, err
+		}
+		return n * ret, nil
+	}
+}
