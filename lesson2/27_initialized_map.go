09f012f5f0dce55724e8d04eac37f8ef258271c9
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-12-10 17:17:40 +0100

    Maps int->string

--- lesson2/27_initialized_map.go
+++ lesson2/27_initialized_map.go
@@ -6,10 +6,10 @@ func main() {
 	var m1 map[int]string = make(map[int]string)
 	fmt.Println(m1)
 
-	m1[0] = "one"
-	m1[1] = "two"
-	m1[2] = "three"
-	m1[3] = "four"
+	m1[1] = "one"
+	m1[2] = "two"
+	m1[3] = "three"
+	m1[4] = "four"
 
 	fmt.Println(m1)
 }
