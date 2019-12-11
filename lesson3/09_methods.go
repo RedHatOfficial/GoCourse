d883b86ae4c7a944778796ec6b397366e00843d3
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-12-11 12:12:54 +0100

    Better naming

--- lesson3/09_methods.go
+++ lesson3/09_methods.go
@@ -5,17 +5,17 @@ import (
 	"math"
 )
 
-type Line struct {
+type LineType struct {
 	x1, y1 float64
 	x2, y2 float64
 }
 
-func (line Line) length() float64 {
+func (line LineType) length() float64 {
 	return math.Hypot(line.x1-line.x2, line.y1-line.y2)
 }
 
 func main() {
-	line1 := Line{x1: 0, y1: 0, x2: 100, y2: 100}
+	line1 := LineType{x1: 0, y1: 0, x2: 100, y2: 100}
 	fmt.Println(line1)
 	lineLength := line1.length()
 	fmt.Println(lineLength)
