15abb39542f60c57743165673aa29c31a8e64d4d
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-12-11 12:13:05 +0100

    Better naming

--- lesson3/10_methods_with_parameters.go
+++ lesson3/10_methods_with_parameters.go
@@ -2,12 +2,12 @@ package main
 
 import "fmt"
 
-type Line struct {
+type LineType struct {
 	x1, y1 float64
 	x2, y2 float64
 }
 
-func (line Line) translate(dx, dy float64) {
+func (line LineType) translate(dx, dy float64) {
 	fmt.Printf("Translating line %v by %f %f\n", line, dx, dy)
 	line.x1 += dx
 	line.y1 += dy
@@ -16,7 +16,7 @@ func (line Line) translate(dx, dy float64) {
 }
 
 func main() {
-	line1 := Line{x1: 0, y1: 0, x2: 100, y2: 100}
+	line1 := LineType{x1: 0, y1: 0, x2: 100, y2: 100}
 	fmt.Println(line1)
 	line1.translate(5, 5)
 	fmt.Println(line1)
