190c52726928f2467a4b38ec874bb36e3c667ecd
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-12-23 10:11:02 +0100

    Matrix library, example #19

---
+++ lesson7/gonum/mat19.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"gonum.org/v1/gonum/mat"
+)
+
+func main() {
+	m := mat.NewDense(3, 3, []float64{0, -1, 0, 1, 0, 0, 0, 0, 1})
+	v3 := mat.NewVecDense(3, []float64{2, 3, 4})
+	v := mat.NewVecDense(3, nil)
+	v.MulVec(m, v3)
+
+	fmt.Printf("v:\n%v\n\n", mat.Formatted(v))
+}
