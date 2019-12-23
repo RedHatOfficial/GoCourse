5f22595ec5e7002c710497603083528083bd1735
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-12-23 10:11:01 +0100

    Matrix library, example #3

---
+++ lesson7/gonum/mat03.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"gonum.org/v1/gonum/mat"
+)
+
+func main() {
+	big := mat.NewDense(100, 100, nil)
+	for i := 0; i < 100; i++ {
+		big.Set(i, i, 1)
+	}
+
+	fmt.Printf("%v\n", big)
+}
