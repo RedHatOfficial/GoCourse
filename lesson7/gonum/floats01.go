48bf0f199f9798df9fed71e96bf34252f60b42ee
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-12-25 10:02:42 +0100

    Add Prod() function

--- lesson7/gonum/floats01.go
+++ lesson7/gonum/floats01.go
@@ -8,8 +8,9 @@ import (
 func main() {
 	x := []float64{1, 2, 3, 4, 5, 6}
 
-	fmt.Printf("x:   %v\n", x)
-	fmt.Printf("Min: %f\n", floats.Min(x))
-	fmt.Printf("Max: %f\n", floats.Max(x))
-	fmt.Printf("Sum: %f\n", floats.Sum(x))
+	fmt.Printf("x:    %v\n", x)
+	fmt.Printf("Min:  %5.0f\n", floats.Min(x))
+	fmt.Printf("Max:  %5.0f\n", floats.Max(x))
+	fmt.Printf("Sum:  %5.0f\n", floats.Sum(x))
+	fmt.Printf("Prod: %5.0f\n", floats.Prod(x))
 }
