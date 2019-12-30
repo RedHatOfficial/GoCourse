ca7e477f28704b664da1f69fdbd024f30e69504f
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-12-30 09:35:20 +0100

    Multiply two float64 slices

---
+++ lesson7/gonum/floats07.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"gonum.org/v1/gonum/floats"
+)
+
+func main() {
+	x := []float64{1, 2, 3, 4, 5, 6}
+	y := []float64{10, 20, 30, 40, 50, 60}
+
+	fmt.Printf("x:   %v\n", x)
+	fmt.Printf("y:   %v\n\n", y)
+
+	floats.Mul(x, y)
+	fmt.Printf("x*y: %v\n", x)
+}
