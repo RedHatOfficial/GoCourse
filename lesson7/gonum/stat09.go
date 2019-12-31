e74b9b59ad14abb9e01dec33467f7f4df89f422c
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-12-31 11:39:30 +0100

    Linear regression computation for semi-random values

---
+++ lesson7/gonum/stat09.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"gonum.org/v1/gonum/stat"
+	"math/rand"
+)
+
+const values = 10000
+
+func main() {
+	xs := [values]float64{}
+	ys := [values]float64{}
+
+	for i := 0; i < len(xs); i++ {
+		xs[i] = float64(i)
+	}
+
+	for i := 0; i < len(ys); i++ {
+		ys[i] = 50.0 - float64(i) + 2.0*rand.Float64() - 1.0
+	}
+
+	offset, slope := stat.LinearRegression(xs[:], ys[:], nil, false)
+	fmt.Printf("Offset:  %f\n", offset)
+	fmt.Printf("Slope:   %f\n", slope)
+}
