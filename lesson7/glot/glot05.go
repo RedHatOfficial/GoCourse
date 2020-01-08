e2beaec2137749863fabcababd066176d8a86a88
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-01-08 09:15:07 +0100

    Setting up renderer based on Cairo

---
+++ lesson7/glot/glot05.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/Arafatk/glot"
+)
+
+func main() {
+	plot, err := glot.NewPlot(2, false, false)
+	if err != nil {
+		panic(err)
+	}
+	defer plot.Close()
+
+	plot.AddPointGroup("Measured data", "lines", []float32{1.2, 1.9, 3.9, 8.2, 9.5, 8.1, 4.1, 1.8, 1.5})
+	plot.AddPointGroup("Expected data", "lines", []float32{1, 2, 4, 8, 9, 8, 4, 2, 1})
+
+	plot.SetXrange(-2, 10)
+	plot.SetYrange(0, 10)
+	plot.Cmd("set terminal pngcairo")
+	plot.Cmd("set output 'glot05.png'")
+	plot.Cmd("replot")
+}
