1f4f769cda50b20111b1b0d8e436bb71f5b7bad2
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-01-31 11:47:38 +0100

    Fixed issues #30, #31, and #32

--- lesson3/13_interface.go
+++ lesson3/13_interface.go
@@ -1,12 +1,15 @@
 package main
 
+// Shape represents any 2D shape drawn in 2D plane
 type Shape interface {
 }
 
+// OpenShape represents an "open" 2D shape, such as line, arc, spline etc.
 type OpenShape interface {
 	length() float64
 }
 
+// ClosedShape represents a "closed" 2D shape, such as square, circle, and ellipse
 type ClosedShape interface {
 	area() float64
 	perimeter() float64
