b78422878c09bc491f9cd64d272ba9225fba36ca
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-01-31 12:13:21 +0100

    Fixed issues #43, #44 and #45

--- lesson3/17_empty_interface.go
+++ lesson3/17_empty_interface.go
@@ -1,9 +1,12 @@
 package main
 
+// I represents a new interface type (in this case empty interface)
 type I interface{}
 
+// T represents an user-defined data type
 type T struct{}
 
+// M is method defined for a data type T
 func (T) M() {
 	println("Nothing happened...")
 }
