b8625b6fdbfe76719d5765da8e2729f3c17b5a96
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-05-05 08:52:51 +0200

    Example #7: blitting via SDL_BlitScaled function

---
+++ lesson7/sdl/introduction/test07.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"github.com/veandco/go-sdl2/img"
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+const (
+	width  = 640
+	height = 480
+)
+
+func main() {
+	if err := sdl.Init(sdl.INIT_VIDEO); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+
+	window, err := sdl.CreateWindow("SDL2 example #7", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		width, height, sdl.WINDOW_SHOWN)
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+
+	primarySurface, err := window.GetSurface()
+	if err != nil {
+		panic(err)
+	}
+
+	tempImage, err := img.Load("test1.bmp")
+	if err != nil {
+		panic(err)
+	}
+	defer tempImage.Free()
+
+	convertedImage, err := tempImage.Convert(primarySurface.Format, 0)
+	if err != nil {
+		panic(err)
+	}
+	defer convertedImage.Free()
+
+	primarySurface.FillRect(nil, sdl.MapRGB(primarySurface.Format, 192, 255, 192))
+
+	err = convertedImage.BlitScaled(nil, primarySurface, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	window.UpdateSurface()
+
+	sdl.Delay(5000)
+}
