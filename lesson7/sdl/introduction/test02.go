9a7ae65d884ce3ffe960b8711ea22a4111d60390
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-05-05 08:52:51 +0200

    Example #2: usage of SDL_UpdateWindowSurfaceRects function

---
+++ lesson7/sdl/introduction/test02.go
@@ -0,0 +1,40 @@
+package main
+
+import "github.com/veandco/go-sdl2/sdl"
+
+const (
+	width  = 640
+	height = 480
+)
+
+func main() {
+	if err := sdl.Init(sdl.INIT_VIDEO); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+
+	window, err := sdl.CreateWindow("SDL2 example #2", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		width, height, sdl.WINDOW_SHOWN)
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+
+	primarySurface, err := window.GetSurface()
+	if err != nil {
+		panic(err)
+	}
+	primarySurface.FillRect(nil, sdl.MapRGB(primarySurface.Format, 192, 255, 192))
+	sdl.Delay(1000)
+
+	var rects = []sdl.Rect{
+		sdl.Rect{
+			X: width / 4,
+			Y: height / 4,
+			W: width / 2,
+			H: height / 2,
+		},
+	}
+	window.UpdateSurfaceRects(rects)
+	sdl.Delay(5000)
+}
