7d3e1c4a48d6cabeda7a68542e773b880cc5598f
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-05-08 08:53:11 +0200

    Fixed issue #56

--- lesson6/web_server_headers.go
+++ lesson6/web_server_headers.go
@@ -7,9 +7,9 @@ import (
 
 func main() {
 	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
-		content_type := req.Header.Get("Content-type")
-		if content_type != "application/json" {
-			http.Error(w, fmt.Sprintf("Unexpected content %s", content_type), http.StatusBadRequest)
+		contentType := req.Header.Get("Content-type")
+		if contentType != "application/json" {
+			http.Error(w, fmt.Sprintf("Unexpected content %s", contentType), http.StatusBadRequest)
 		}
 		// parse ...
 	})
