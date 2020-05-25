f1c6411d2562b74bac2bf4894960595925a35a53
Dmitry Volodin <dmvolod@gmail.com>
2020-05-25 15:38:40 +0300

    Fix lesson6 slide5 code and display

--- lesson6/web_server_more_control.go
+++ lesson6/web_server_more_control.go
@@ -2,23 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"time"
 )
 
 func main() {
 	mux := http.NewServeMux()
+	addr := "localhost:8080"
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		log.Printf("Handling request at %s", req.URL.Path)
 		fmt.Fprintf(w, "Hello from %s\n", req.URL.Path)
 	})
 	server := &http.Server{
-		Addr:           "localhost:8080",
+		Addr:           addr,
 		Handler:        mux,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
+
+	log.Printf("Running server at %s", addr)
 	server.ListenAndServe()
 }
