0c7a43ea926c599d57153e6a4e03bda9627d687d
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-29 09:17:33 +0200

    Example #5: get string value

---
+++ lesson7/redis/05_get_string/05_get_string.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/go-redis/redis/v8"
+)
+
+// adresa určující službu Redisu, která se má použít
+const redisAddress = "localhost:6379"
+
+func main() {
+	// vytvoření nového klienta s předáním konfiguračních parametrů
+	client := redis.NewClient(&redis.Options{
+		Addr:     redisAddress,
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	// neměli bychom zapomenout na ukončení práce s klientem
+	defer func() {
+		err := client.Close()
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	// získáme kontext
+	context := client.Context()
+
+	// přečtení hodnoty z databáze Redisu
+	address, err := client.Get(context, "Seriál o jazyku Go").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Adresa:", address)
+}
