3bc9fe7eb0b037e2427fabd2c5033a5604f5e1a8
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-02 08:03:41 +0200

    Example #14: marshaling array of structs into XML, node names + other improvements

---
+++ lesson7/marshalling/14_xml_marshal_array_of_struct_4.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+)
+
+type User struct {
+	Id      uint32 `xml:"id"`
+	Name    string `xml:"user_name"`
+	Surname string `xml:"surname"`
+}
+
+type Users struct {
+	XMLName xml.Name `xml:"users"`
+	List    []User   `xml:"user"`
+}
+
+func main() {
+	var users Users = Users{
+		List: []User{
+			User{
+				Id:      1,
+				Name:    "Pepek",
+				Surname: "Vyskoč"},
+			User{
+				Id:      2,
+				Name:    "Pepek",
+				Surname: "Vyskoč"},
+			User{
+				Id:      3,
+				Name:    "Josef",
+				Surname: "Vyskočil"},
+		},
+	}
+
+	usersAsXML, _ := xml.MarshalIndent(users, "", "    ")
+	fmt.Println(string(usersAsXML))
+}
