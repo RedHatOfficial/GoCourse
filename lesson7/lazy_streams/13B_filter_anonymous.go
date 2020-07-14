cce5f77e9c7ce1a96c8d7412f9a9a0fcf81f59b5
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-14 09:16:17 +0200

    Filter method for anonymous type

---
+++ lesson7/lazy_streams/13B_filter_anonymous.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/wesovilabs/koazee"
+)
+
+func main() {
+	values := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Printf("input: %v\n", values)
+
+	stream1 := koazee.StreamOf(values)
+	stream2 := stream1.Filter(func(x int) bool { return x%2 == 0 }).Do()
+	stream3 := stream1.Filter(func(x int) bool { return x%3 == 0 }).Do()
+
+	fmt.Printf("stream1:  %v\n", stream1.Out().Val())
+	fmt.Printf("stream2:  %v\n", stream2.Out().Val())
+	fmt.Printf("stream3:  %v\n", stream3.Out().Val())
+}
