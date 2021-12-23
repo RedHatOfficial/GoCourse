5f6088d841b46437efdd1a8fefa03fbe1843d633
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-12-23 09:42:17 +0100

    Combine parameters type

--- lesson3/03_functions_as_values.go
+++ lesson3/03_functions_as_values.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 )
 
-func testBinaryOps(a int, b int) {
-	f1 := func(a int, b int) int { return a + b }
-	f2 := func(a int, b int) int { return a - b }
-	f3 := func(a int, b int) int { return a * b }
-	f4 := func(a int, b int) int { return a / b }
+func testBinaryOps(a, b int) {
+	f1 := func(a, b int) int { return a + b }
+	f2 := func(a, b int) int { return a - b }
+	f3 := func(a, b int) int { return a * b }
+	f4 := func(a, b int) int { return a / b }
 	fmt.Printf("%d + %d = %d\n", a, b, f1(a, b))
 	fmt.Printf("%d - %d = %d\n", a, b, f2(a, b))
 	fmt.Printf("%d * %d = %d\n", a, b, f3(a, b))
