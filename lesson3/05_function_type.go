083f92e957597de017edb5467596ce37da24beb6
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-12-23 09:40:24 +0100

    Combine parameters type

--- lesson3/05_function_type.go
+++ lesson3/05_function_type.go
@@ -11,11 +11,11 @@ func applyBinaryOp(a, b int, operation BinaryOp) int {
 	return operation(a, b)
 }
 
-func testBinaryOps(a int, b int) {
-	f1 := func(a int, b int) int { return a + b }
-	f2 := func(a int, b int) int { return a - b }
-	f3 := func(a int, b int) int { return a * b }
-	f4 := func(a int, b int) int { return a / b }
+func testBinaryOps(a, b int) {
+	f1 := func(a, b int) int { return a + b }
+	f2 := func(a, b int) int { return a - b }
+	f3 := func(a, b int) int { return a * b }
+	f4 := func(a, b int) int { return a / b }
 	fmt.Printf("%d + %d = %d\n", a, b, applyBinaryOp(a, b, f1))
 	fmt.Printf("%d - %d = %d\n", a, b, applyBinaryOp(a, b, f2))
 	fmt.Printf("%d * %d = %d\n", a, b, applyBinaryOp(a, b, f3))
