e0abc314c06b93df6d415923c41d1268cc194dce
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-12-28 09:15:44 +0100

    Docstrings for exported items

--- lesson7/marshalling/12_xml_marshal_array_of_struct_2.go
+++ lesson7/marshalling/12_xml_marshal_array_of_struct_2.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 )
 
+// User struct represent one item to be marshaled into XML
 type User struct {
 	Id      uint32 `xml:"id"`
 	Name    string `xml:"user_name"`
 	Surname string `xml:"surname"`
 }
 
+// Users struct represents list of items to be marshaled into XML
 type Users struct {
 	XMLName xml.Name `xml:"users"`
 	List    []User
