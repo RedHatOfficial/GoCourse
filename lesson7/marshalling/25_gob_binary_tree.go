ce61b64d42582bc92f621387e7d354cef518da7a
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-12 07:58:07 +0100

    Docstring for Insert function

--- lesson7/marshalling/25_gob_binary_tree.go
+++ lesson7/marshalling/25_gob_binary_tree.go
@@ -23,6 +23,7 @@ type BinaryTree struct {
 	root *Node
 }
 
+// Insert methods inserts new node into binary tree
 func (bt *BinaryTree) Insert(value Item) {
 	node := &Node{value, nil, nil}
 	if bt.root == nil {
