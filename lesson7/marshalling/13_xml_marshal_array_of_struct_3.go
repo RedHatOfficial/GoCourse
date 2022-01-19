48b8a0f6abc8c35ea4fd9818308d075a59be562a
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-19 08:18:49 +0100

    Id -> ID

--- lesson7/marshalling/13_xml_marshal_array_of_struct_3.go
+++ lesson7/marshalling/13_xml_marshal_array_of_struct_3.go
@@ -8,7 +8,7 @@ import (
 // User struct represent one item to be marshalled into XML
 type User struct {
 	XMLName xml.Name `xml:"user"`
-	Id      uint32   `xml:"id"`
+	ID      uint32   `xml:"id"`
 	Name    string   `xml:"user_name"`
 	Surname string   `xml:"surname"`
 }
@@ -23,15 +23,15 @@ func main() {
 	var users Users = Users{
 		List: []User{
 			User{
-				Id:      1,
+				ID:      1,
 				Name:    "Pepek",
 				Surname: "Vyskoč"},
 			User{
-				Id:      2,
+				ID:      2,
 				Name:    "Pepek",
 				Surname: "Vyskoč"},
 			User{
-				Id:      3,
+				ID:      3,
 				Name:    "Josef",
 				Surname: "Vyskočil"},
 		},
