dabdc6d71f3b5c2e402a1161f74d078a23a9b30c
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-13 08:50:50 +0200

    Example #1: simple print() function

---
+++ lesson8/01_print.go
@@ -0,0 +1,14 @@
+// The good old "normal" function as used in several
+// programming languages for decades.
+
+package main
+
+import "fmt"
+
+func printValue(value string) {
+	fmt.Println(value)
+}
+
+func main() {
+	printValue("www.root.cz")
+}
