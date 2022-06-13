13f2c6d50acdb07056a3549a2623990c19a85024
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-13 08:50:50 +0200

    Pow functions - not generic

---
+++ lesson8/21_pow_floats_B.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func pow32(x float32, y float32) float32 {
+	return float32(math.Pow(float64(x), float64(y)))
+}
+
+func pow64(x float64, y float64) float64 {
+	return math.Pow(x, y)
+}
+
+func main() {
+	for x := float32(0.0); x < 5.0; x += 0.5 {
+		result := pow32(x, 2)
+		fmt.Printf("%T %v\n", result, result)
+	}
+
+	fmt.Println()
+
+	for x := 0.0; x < 5.0; x += 0.5 {
+		result := pow64(x, 2)
+		fmt.Printf("%T %v\n", result, result)
+	}
+}
