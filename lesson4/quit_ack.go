c630469e010eb4e777fdbd8bc6fe41fe82179414
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-10-31 14:03:09 +0100

    Quit+ack communication

---
+++ lesson4/quit_ack.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+import "time"
+
+func worker(data chan int, quit chan int, ack chan int) {
+	for {
+		select {
+		case x := <-data:
+			fmt.Println("start working on", x)
+			time.Sleep(1 * time.Second)
+			fmt.Println("finish working on", x)
+		case <-quit:
+			fmt.Println("finishing...")
+			time.Sleep(4 * time.Second)
+			fmt.Println("...done")
+			ack <- 1
+			return
+		}
+	}
+}
+
+func main() {
+	data := make(chan int)
+	quit := make(chan int)
+	ack := make(chan int)
+
+	go worker(data, quit, ack)
+
+	data <- 1
+	quit <- 1
+	<-ack
+	fmt.Println("All done")
+}
