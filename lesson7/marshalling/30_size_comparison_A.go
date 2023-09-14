bcf3e1646d59930afe3a72e2cf4741ba841a67c9
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-09-14 10:09:28 +0200

    New octal literal style

--- lesson7/marshalling/30_size_comparison_A.go
+++ lesson7/marshalling/30_size_comparison_A.go
@@ -6,8 +6,9 @@ import (
 	"encoding/json"
 	"encoding/xml"
 	"fmt"
-	"gopkg.in/mgo.v2/bson"
 	"os"
+
+	"gopkg.in/mgo.v2/bson"
 )
 
 // Vector represents type of data to be serialized into various formats
@@ -70,7 +71,7 @@ func encodeVectorIntoGob(vector Vector) ([]byte, error) {
 }
 
 func saveVector(encodedVector []byte, filename string) {
-	err := os.WriteFile(filename, encodedVector, 0644)
+	err := os.WriteFile(filename, encodedVector, 0o644)
 	if err != nil {
 		fmt.Println(err)
 	} else {
