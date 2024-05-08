f09454676ed27741dcf456d018d26e3a89fc120b
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-05-08 12:50:26 +0200

    Dynamic library loading

---
+++ go_python/example1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+
+#include "so1.h"
+
+int main()
+{
+    void *library;
+    void (*hello)();
+
+    library = dlopen("./so1.so", RTLD_LAZY);
+    if (library != NULL) {
+        printf("dynamic library loaded: %p\n", library);
+    } else {
+        puts("unable to load dynamic library");
+        return 1;
+    }
+
+    hello = dlsym(library, "hello");
+    hello = dlsym(library, "hello");
+
+    if (hello != NULL) {
+        printf("address for 'hello' retrieved: %p\n", (void*)hello);
+        puts("Calling 'hello'...");
+        hello();
+        puts("...called");
+    } else {
+        puts("unable to retrieve address for 'hello'");
+    }
+
+
+    if (library != NULL) {
+        int err = dlclose(library);
+        if (err != 0) {
+            puts("unable to close dynamic library");
+            return 1;
+        } else {
+            puts("dynamic library closed");
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
