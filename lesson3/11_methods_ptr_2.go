a07179cb2427a4c25015fd1488fa2b7f7deafbe9
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-05-30 15:50:24 +0200

    Line variants

---
+++ lesson3/11_methods_ptr_2.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// Line represents a line in 2D plane
+type Line struct {
+	x1, y1 float64
+	x2, y2 float64
+}
+
+func (line *Line) translate(dx, dy float64) {
+	fmt.Printf("Translating line %v by %f %f\n", *line, dx, dy)
+	line.x1 += dx
+	line.y1 += dy
+	line.x2 += dx
+	line.y2 += dy
+}
+
+func main() {
+	line1 := Line{x1: 0, y1: 0, x2: 100, y2: 100}
+	fmt.Println(line1)
+	line1.translate(5, 5)
+	fmt.Println(line1)
+}
