571e69db3047e83cd9adfc060d69d240d8b4498b
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-05-31 16:40:13 +0200

    Removed comments

--- lesson6/metrics.go
+++ lesson6/metrics.go
@@ -1,16 +1,3 @@
-// Seriál "Programovací jazyk Go"
-//    https://www.root.cz/serialy/programovaci-jazyk-go/
-//
-// Třicátá sedmá část
-//    Monitoring služeb a mikroslužeb psaných v Go nástrojem Prometheus
-//    https://www.root.cz/clanky/monitoring-sluzeb-a-mikrosluzeb-psanych-v-go-nastrojem-prometheus/
-//
-// Repositář:
-//    https://github.com/tisnik/go-root/
-//
-// Seznam demonstračních příkladů ze třicáté sedmé části:
-//    https://github.com/tisnik/go-root/blob/master/article_37/README.md
-
 package main
 
 import (
