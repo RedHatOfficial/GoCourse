80676b1298f309e795d85afa8eeaae39aff77124
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-12-02 08:50:55 +0100

    Marshalling data struct with indentation into JSON

---
+++ lesson11/json_marshal_struct_indent.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type User struct {
+	ID      uint32 `json:"user_id"`
+	Name    string `json:"user_name"`
+	Surname string `json:"surname"`
+}
+
+func main() {
+	user := User{
+		ID:      1,
+		Name:    "John",
+		Surname: "Doe"}
+
+	userAsJSON, _ := json.MarshalIndent(user, "", "    ")
+	fmt.Println(string(userAsJSON))
+}
