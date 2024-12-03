872ad905010214f31b9a4e59b3e10ad2d552adec
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-12-03 08:20:37 +0100

    Marshalling special floats values into JSON

---
+++ lesson11/json_marshal_floats_specvalues.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"math"
+)
+
+func main() {
+	var a float64 = -0.0
+	var b float64 = math.NaN()
+	var c float64 = -math.NaN()
+	var d float64 = math.Inf(1)
+	var e float64 = math.Inf(-1)
+
+	var jsonOutput []byte
+
+	jsonOutput, _ = json.Marshal(a)
+	fmt.Println(string(jsonOutput))
+
+	jsonOutput, _ = json.Marshal(b)
+	fmt.Println(string(jsonOutput))
+
+	jsonOutput, _ = json.Marshal(c)
+	fmt.Println(string(jsonOutput))
+
+	jsonOutput, _ = json.Marshal(d)
+	fmt.Println(string(jsonOutput))
+
+	jsonOutput, _ = json.Marshal(e)
+	fmt.Println(string(jsonOutput))
+}
