405d9eeba94d29ea189b1ce73a954e7a3d398408
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-12-05 08:40:26 +0100

    Unmarshal JSON

---
+++ lesson11/json_umarshal_lowercase.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type User struct {
+	ID      uint32 `json:"user_id"`
+	Name    string `json:"user_name"`
+	surname string `json:"surname"`
+}
+
+func main() {
+	user := User{
+		ID:      1,
+		Name:    "John",
+		surname: "Doe"}
+
+	userAsJSON, _ := json.MarshalIndent(user, "", "    ")
+
+	var user2 User
+	fmt.Println(user2)
+
+	err := json.Unmarshal(userAsJSON, &user2)
+	fmt.Println(err)
+	fmt.Println(user2)
+}
