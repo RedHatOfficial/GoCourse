2fb2bc4f27554852f857d6e031ee059bb8c4f841
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-12-30 18:57:56 +0100

    DOM manipulation

---
+++ lesson12/dom_manipulation.go
@@ -0,0 +1,31 @@
+// Technologie WebAssembly
+//
+// - manipulace s DOMem přímo z jazyka Go
+// - změna atributu vybraného elementu
+
+package main
+
+import (
+	"fmt"
+	"syscall/js"
+)
+
+func main() {
+	fmt.Println("started")
+
+	// získání objektu typu "window" (z pohledu JavaScriptu)
+	window := js.Global()
+
+	// přečtení instance objektu "document"
+	document := window.Get("document")
+
+	// získání reference na element s ID="header" umístěného
+	// na HTML stránce
+	element := document.Call("getElementById", "header")
+
+	// změna atributu elementu
+	// (text uvnitř značky)
+	element.Set("innerHTML", "foobar")
+
+	fmt.Println("finished")
+}
