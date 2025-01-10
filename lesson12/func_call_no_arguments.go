765020576569956dba9bdacf0809358f138254b6
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-10 16:08:08 +0100

    Function call without arguments, Go part

---
+++ lesson12/func_call_no_arguments.go
@@ -0,0 +1,34 @@
+// Technologie WebAssembly
+//
+// - rozhraní mezi jazyky Go a JavaScript
+
+package main
+
+import (
+	"fmt"
+	"syscall/js"
+)
+
+// funkce, která se bude volat z HTML stránky, jakoby
+// se jednalo o JavaScriptovou funkci
+func PrintHello(this js.Value, args []js.Value) any {
+	fmt.Println("function PrintHello called")
+
+	// je nutné vrátit nějakou hodnotu
+	return nil
+}
+
+func main() {
+	fmt.Println("started")
+
+	c := make(chan bool)
+
+	// export funkce PrintHello tak, aby byla volatelná
+	// z JavaScriptu
+	js.Global().Set("printHello", js.FuncOf(PrintHello))
+
+	// realizace nekonečného čekání
+	<-c
+
+	fmt.Println("finished")
+}
