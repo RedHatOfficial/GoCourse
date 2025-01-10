2008bbeadf8854720723f6c284b46cce097d7d85
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-10 16:08:08 +0100

    Function part with arguments, Go part

---
+++ lesson12/func_call_with_arguments.go
@@ -0,0 +1,39 @@
+// Technologie WebAssembly
+//
+// - rozhraní mezi jazyky Go a JavaScript
+
+package main
+
+import (
+	"fmt"
+	"syscall/js"
+)
+
+// funkce, která se bude volat z HTML stránky, jakoby
+// se jednalo o JavaScriptovou funkci
+func PrintArguments(this js.Value, args []js.Value) any {
+	fmt.Println("function PrintArguments called")
+	fmt.Printf("# of parameters: %d\n", len(args))
+
+	for i, arg := range args {
+		fmt.Printf("parameter # %d: %v\n", i, arg)
+	}
+
+	// je nutné vrátit nějakou hodnotu
+	return nil
+}
+
+func main() {
+	fmt.Println("started")
+
+	c := make(chan bool)
+
+	// export funkce PrintArguments tak, aby byla volatelná
+	// z JavaScriptu
+	js.Global().Set("printArguments", js.FuncOf(PrintArguments))
+
+	// realizace nekonečného čekání
+	<-c
+
+	fmt.Println("finished")
+}
