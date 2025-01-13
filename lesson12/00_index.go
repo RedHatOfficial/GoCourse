fb33de750f934217ffbcb2a3faa5ca0c40b7a70d
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-13 12:07:42 +0100

    Source list

--- lesson12/00_index.go
+++ lesson12/00_index.go
@@ -1,2 +1,16 @@
 // GopherJS transpiler
 //
+// dom_add_element.go
+// dom_manipulation.go
+// draw_into_canvas.go
+// hello_world_2.go
+// hello_world.go
+// http_server.go
+// js_interop_1.go
+// js_interop_2.go
+// func_call_no_arguments.go
+// func_call_with_arguments.go
+// func_call_argument_types_1.go
+// func_call_argument_types_2.go
+// func_call_two_floats.go
+// func_call_two_ints.go
