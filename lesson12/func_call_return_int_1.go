1132aefd424245055324f4cde8a6014b188436d1
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-14 19:16:11 +0100

    Returning value from Go function to JavaScript one, 1st example

---
+++ lesson12/func_call_return_int_1.go
@@ -0,0 +1,60 @@
+// Technologie WebAssembly
+//
+// - rozhraní mezi jazyky Go a JavaScript
+// - kontrola počtu argumentů předaných funkci CalcSum
+// - kontrola typu argumentů předaných funkci CalcSum
+// - provedení konverze na nativní typy jazyka Go
+// - převedení výsledku zpět na JavaScriptový typ
+
+package main
+
+import (
+	"fmt"
+	"syscall/js"
+)
+
+// funkce, která se bude volat z HTML stránky, jakoby
+// se jednalo o JavaScriptovou funkci
+func CalcSum(this js.Value, args []js.Value) any {
+	// kontrola počtu předaných argumentů
+	if len(args) != 2 {
+		fmt.Printf("incorrect number of arguments %d, but just two are accepted\n", len(args))
+		return nil
+	}
+
+	// kontrola typu předaných argumentů
+	// (pozor - původní syntaxe zápisu smyčky)
+	for index := 0; index < 2; index++ {
+		typ := args[index].Type()
+		if typ != js.TypeNumber {
+			fmt.Printf("Argument #%d has incorrect type %s\n", index, typ.String())
+			return nil
+		}
+	}
+
+	// počet i typ argumentů je korektní
+	// lze tedy provést jejich konverzi
+	x := args[0].Int()
+	y := args[1].Int()
+
+	// vypočítat výsledek
+	z := x + y
+
+	// vrátit výsledek s explicitní konverzí
+	return js.ValueOf(z)
+}
+
+func main() {
+	fmt.Println("started")
+
+	c := make(chan bool)
+
+	// export funkce CalcSum tak, aby byla volatelná
+	// z JavaScriptu
+	js.Global().Set("calcSum", js.FuncOf(CalcSum))
+
+	// realizace nekonečného čekání
+	<-c
+
+	fmt.Println("finished")
+}
