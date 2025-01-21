18345a0ddcdff19c65b58847e265ea9960214eb0
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-21 16:35:22 +0100

    Function call, pass JavaScript object to it

---
+++ lesson12/func_call_object_1.go
@@ -0,0 +1,54 @@
+// Technologie WebAssembly a GopherJS
+//
+// - rozhraní mezi jazyky Go a JavaScript
+// - kontrola počtu argumentů předaných funkci PrintObject
+// - kontrola typu argumentů předaných funkci PrintObject
+
+package main
+
+import (
+	"fmt"
+	"syscall/js"
+)
+
+// funkce, která se bude volat z HTML stránky, jakoby
+// se jednalo o JavaScriptovou funkci
+func PrintObject(this js.Value, args []js.Value) any {
+	// kontrola počtu předaných argumentů
+	if len(args) != 1 {
+		fmt.Printf("incorrect number of arguments %d, but exactly one is accepted\n", len(args))
+		return nil
+	}
+
+	object := args[0]
+
+	// kontrola typu předaného argumentu
+	typ := object.Type()
+	if typ != js.TypeObject {
+		fmt.Printf("Argument #0 has incorrect type %s\n", typ.String())
+		return nil
+	}
+
+	// zobrazit objekt
+	fmt.Println(object)
+
+	// je nutné vrátit nějakou hodnotu
+	return nil
+}
+
+func main() {
+	fmt.Println("started")
+
+	c := make(chan bool)
+
+	// export funkce PrintObject tak, aby byla volatelná
+	// z JavaScriptu
+	js.Global().Set("printObject", js.FuncOf(PrintObject))
+
+	// realizace nekonečného čekání
+	// (nutno provést při překladu do WebAssembly, ktežto
+	// v případě použití GopherJS je možné hlavní funkci ukončit)
+	<-c
+
+	fmt.Println("finished")
+}
