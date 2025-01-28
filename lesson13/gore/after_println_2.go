29bc7842c182b8adcba9d653dc010a81b89d8795
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-28 20:43:35 +0100

    Source code after println call

---
+++ lesson13/gore/after_println_2.go
@@ -0,0 +1,10 @@
+package main
+
+import "github.com/k0kubun/pp/v3"
+
+func __gore_p(xs ...any) {
+    for _, x := range xs {
+        pp.Println(x)
+    }
+}
+func main() { x := 42; println(x); y := -1; println(y) }
