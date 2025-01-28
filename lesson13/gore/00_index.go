32f354797f4142a052bca9846c8de6e6476bf158
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-28 20:43:35 +0100

    List of examples

---
+++ lesson13/gore/00_index.go
@@ -0,0 +1,11 @@
+// List of examples:
+//
+// after_import.go
+// after_println_2.go
+// after_println.go
+// autoimport.go
+// function_declaration.go
+// gore_init.go
+// type_declaration.go
+// type_usage.go
+// variables.go
