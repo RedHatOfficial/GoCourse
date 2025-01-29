e1dcc4237343d4eb682e5b98ecd8ae2b51f5286b
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-29 10:28:53 +0100

    Gomacro script evaluation

---
+++ lesson13/gomacro/gomacro_eval_3.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/cosmos72/gomacro/fast"
+)
+
+const script = `
+x:=6
+y:=7
+z:=x*y
+z
+`
+
+func RunGomacro(script string) any {
+	interp := fast.New()
+	vals, _ := interp.Eval(script)
+	if len(vals) < 1 {
+		return "no value"
+	}
+	return vals[0].ReflectValue()
+}
+
+func main() {
+	fmt.Println(RunGomacro(script))
+}
