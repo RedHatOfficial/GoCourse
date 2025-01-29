ea92fd057000d1e4f39a11250b97cfb0482c37fb
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-29 11:32:04 +0100

    Script evaluation by Yaegi, example #2

---
+++ lesson13/yaegi/yaegi_eval_2.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/traefik/yaegi/interp"
+	"github.com/traefik/yaegi/stdlib"
+)
+
+const script = `
+x:=6
+y:=7
+z:=x*y
+`
+
+func RunYaegi(script string) any {
+	i := interp.New(interp.Options{})
+
+	i.Use(stdlib.Symbols)
+
+	ret, err := i.Eval(script)
+	if err != nil {
+		panic(err)
+	}
+
+	return ret
+}
+
+func main() {
+	fmt.Println(RunYaegi(script))
+}
