330c202c483c0be335c831d37718bf01cbcb8c52
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-04 09:00:07 +0100

    ValueOf for nil values

---
+++ lesson14/values_of_type_nil.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func test_get_type(x any) {
+	value := reflect.ValueOf(x)
+	fmt.Printf("value %v of type %T\n", x, x)
+	fmt.Printf("value %v of type %T\n", value, value)
+	fmt.Println()
+}
+
+type user struct {
+	name    string
+	surname string
+}
+
+func main() {
+	var nil1 *int = nil
+	test_get_type(nil1)
+
+	var nil2 *bool = nil
+	test_get_type(nil2)
+
+	var nil3 *string = nil
+	test_get_type(nil3)
+
+	var nil4 *user = nil
+	test_get_type(nil4)
+}
