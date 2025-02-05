6f06051d6cb2fbdb0cfc7dcc7776d48140d52bed
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-05 13:24:47 +0100

    Get types for nil values

---
+++ lesson14/get_types_nil.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func test_get_type(x any) {
+	value := reflect.ValueOf(x)
+	typ := value.Type()
+	fmt.Println("type is: ", typ)
+}
+
+type user struct {
+	name    string
+	surname string
+}
+
+func main() {
+	var nil1 *int = nil
+	test_get_type(nil1)
+
+	var nil2 *bool = nil
+	test_get_type(nil2)
+
+	var nil3 *string = nil
+	test_get_type(nil3)
+
+	var nil4 *user = nil
+	test_get_type(nil4)
+}
