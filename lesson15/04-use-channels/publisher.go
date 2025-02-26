4de5ee5c1f82cef5cf0e14afb7597875c858dff0
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-26 15:49:33 +0100

    Publisher using channels

---
+++ lesson15/04-use-channels/publisher.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	nats "github.com/nats-io/nats.go"
+)
+
+const Subject = "test1"
+
+func main() {
+	conn, err := nats.Connect(nats.DefaultURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
+	fmt.Printf("Connected to %s\n", nats.DefaultURL)
+
+	econn, err2 := nats.NewEncodedConn(conn, nats.DEFAULT_ENCODER)
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+	defer econn.Close()
+
+	channel := make(chan string)
+	econn.BindSendChan(Subject, channel)
+
+	fmt.Println("Channel created")
+	fmt.Println("Sending 3 messages")
+
+	channel <- "Hello World #1"
+	channel <- "Hello World #2"
+	channel <- "Hello World #3"
+
+	conn.Flush()
+	fmt.Println("All messages sent")
+}
